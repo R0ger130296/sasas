825b6114d48f6caaeac30e294c6ff7f7c8dd21e4
unknown <grarogccee@gmail.com>
2019-06-25 10:49:32 -0500

    practica

--- src/app/leerdatos/leerdatos.component.ts
+++ src/app/leerdatos/leerdatos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵReflectionCapabilities } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-leerdatos',
@@ -7,14 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeerdatosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  http: HttpClient) { }
   TablaSelecionada:number
   tablas:string[]
+   datos:any;
+   campos:string[]
+   verTabla:boolean;
   ngOnInit() {
-    this.tablas=['persona','productos']
+    this.tablas=['productos']
     this.TablaSelecionada=999;
+    
   }
   leerDatos(){
-    alert(`la tabla selecionada es la numero ${this.TablaSelecionada}, ${this.tablas[this.TablaSelecionada]}`)
+    if (this.TablaSelecionada===999){
+      this.verTabla=false;
+    }else{
+      this.verTabla=true;
+    }
+    let url=`http://localhost:8001/server/kawina?tabla=${this.tablas[this.TablaSelecionada]}`
+    this.http.get<any>(url)
+    .subscribe(resultados=>{
+      if(resultados.ok=true){
+      this.datos=resultados.data
+      this.campos=Object.keys(this.datos[0])
+      }else{
+        alert(`la tabla selecionada no tiene datos`)
+      }
+      console.log(this.datos)
+
+    })
+    // alert(`la tabla selecionada es la numero ${this.TablaSelecionada}, ${this.tablas[this.TablaSelecionada]}`)
+    
+    console.log(this.datos)
+    
   }
 }
